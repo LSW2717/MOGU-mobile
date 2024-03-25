3dba02eab9fa16d4caf90b2ae646ad347083717f
Dcool <90443691+LSW2717@users.noreply.github.com>
2024-03-26 01:19:40 +0900

    feat: 로그인 로직 구현

--- lib/common/data_source/data.dart
+++ lib/common/data_source/data.dart
@@ -3,3 +3,7 @@ const REFRESH_TOKEN_KEY = 'REFRESH_TOKEN';
 
 
 const ip = 'localhost:8080';
+
+const url = 'https://samgjhfzlzntqxnabtou.supabase.co';
+
+const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhbWdqaGZ6bHpudHF4bmFidG91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMjExNDcsImV4cCI6MjAyNjc5NzE0N30.NC1EdgxfQCF6Sy350mc4gcRYpVauzDYe9upnDnGzoXs';
